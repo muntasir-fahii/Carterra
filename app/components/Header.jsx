0e4e05fc1e161f749a9fa51a3e4016e30cd32a74
muntasir-fahii <fa3877957@gmail.com>
2023-08-18 00:02:56 +0600

    navbar added

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Link } from "react-scroll";
 import { useMediaQuery } from "react-responsive";
 import logo from "../../public/images/cta/logo.png";
+import { BiMenuAltRight, BiX } from "react-icons/bi";
 
 import SearchMobile from "./SearchMobile";
 
@@ -31,25 +32,105 @@ const Header = () => {
     };
   });
 
-  console.log(header);
-
   return (
     <header
       className={`${
         header ? "bg-white shadow-md py-1" : "bg-transparent shadow-none py-3"
       }  fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
     >
-      <div className="container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
-        <div>
+      <div className="xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex justify-between items-center px-4">
           <Link
             to="home"
             smooth={desktopMode}
             spy={true}
             className="cursor-pointer"
           >
-            <Image src={logo} width={132} height={32} alt="logo" className="" />
+            <Image src={logo} width={132} height={32} alt="logo" />
           </Link>
+          <div
+            onClick={() => {
+              setNav(!nav);
+            }}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl" />
+            )}
+          </div>
         </div>
+        <nav
+          className={`${
+            nav ? "max-h-max py-8 px-4 xl:py-0 xl:px-0" : "max-h-0 xl:max-h-max"
+          } flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bold xl:font-medium xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-300 text-center xl:text-left uppercase text-sm xl:text-[16px] xl:normal-case`}
+        >
+          <Link
+            className="cursor-pointer"
+            to="home"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Home
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="cars"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Cars
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="about"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            About
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="why"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Why us
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="testimonials"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Testimonials
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="contact"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Contact
+          </Link>
+          <Link
+            className="xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto"
+            to="/"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            See all cars
+          </Link>
+          <SearchMobile />
+        </nav>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ module.exports = {
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "1024",
-      xl: "1300",
+      lg: "1024px",
+      xl: "1300px",
     },
     extend: {
       colors: {
