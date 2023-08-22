04429ee9b6de50076c9d5246dc75287801a64860
muntasir-fahii <fa3877957@gmail.com>
2023-08-22 19:28:17 +0600

    location component added

--- app/components/LocationSelection.jsx
+++ app/components/LocationSelection.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Menu } from "@headlessui/react";
-import { FaMapMArketAlt } from "react-icons/fa";
+import { FaMapMArketAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 const locations = [
   "Main street 123, United States",
@@ -11,19 +11,40 @@ const locations = [
 ];
 
 const LocationSelection = () => {
-  const [location, setLocation] = useState("Select Loation");
+  const [location, setLocation] = useState("Select Location");
   return (
     <Menu as="div" className="w-full h-full flex xl:flex-row">
       <div className="relative flex-1">
         <Menu.Button className="dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
           <div className="w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10">
-            <div>
-              <div>Select Location</div>
-              <div>{location}</div>
+            <div className="flex flex-col justify-center">
+              <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
+                <FaMapMarkerAlt className="text-accent" />{" "}
+                <div className="text-[15px] uppercase font-bold">
+                  Select Location
+                </div>
+              </div>
+              <div className="uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left">
+                {location}
+              </div>
             </div>
           </div>
         </Menu.Button>
-        <Menu.Items>Menu Items</Menu.Items>
+        <Menu.Items className="drodown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded=[10px]">
+          {locations.map((location, i) => {
+            return (
+              <div
+                onClick={() => {
+                  setLocation(location);
+                }}
+                key={i}
+                className="cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase"
+              >
+                {location}
+              </div>
+            );
+          })}
+        </Menu.Items>
       </div>
     </Menu>
   );
